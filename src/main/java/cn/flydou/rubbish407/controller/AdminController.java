c3901236a783b184115e4ac1afe80b524846ecf5
Eraser <4210772842@qq.com>
2017-06-06 11:11:35 +0800

    加入AdminLTE后台管理模版，并且加入管理员基本管理模版

---
+++ src/main/java/cn/flydou/rubbish407/controller/AdminController.java
@@ -0,0 +1,15 @@
+package cn.flydou.rubbish407.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Eraser on 2017/6/6.
+ */
+@Controller
+public class AdminController {
+    @RequestMapping("/admin")
+    public String index(){
+        return "home";
+    }
+}
