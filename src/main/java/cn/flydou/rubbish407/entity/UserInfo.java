57337a8947f6de23ce2ad4810c23bedd98d1474f
wanchenqi <346260388@qq.com>
2017-06-08 19:32:40 +0800

    Mybatis Mapper UserInfo

---
+++ src/main/java/cn/flydou/rubbish407/entity/UserInfo.java
@@ -0,0 +1,175 @@
+package cn.flydou.rubbish407.entity;
+
+/**
+ * Created by wanchenqi on 2017/6/8.
+ */
+public class UserInfo {
+    private int id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setIdcardNum(String idcardNum) {
+        this.idcardNum = idcardNum;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public void setNativePlace(String nativePlace) {
+        this.nativePlace = nativePlace;
+    }
+
+    public void setRegisterBatch(String registerBatch) {
+        this.registerBatch = registerBatch;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setJobState(String jobState) {
+        this.jobState = jobState;
+    }
+
+    public void setTelNum1(String telNum1) {
+        this.telNum1 = telNum1;
+    }
+
+    public void setTelNum2(String telNum2) {
+        this.telNum2 = telNum2;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    private int userId;
+    private String name;
+    private String idcardNum;
+    private String sex;
+    private int num;
+    private int age;
+    private String apartment;
+    private String nativePlace;
+    private String registerBatch;
+    private String date;
+    private String jobState;
+    private String telNum1;
+    private String telNum2;
+    private String qq;
+    private String email;
+    private String address;
+    private String job;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIdcardNum() {
+        return idcardNum;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public String getNativePlace() {
+        return nativePlace;
+    }
+
+    public String getRegisterBatch() {
+        return registerBatch;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getJobState() {
+        return jobState;
+    }
+
+    public String getTelNum1() {
+        return telNum1;
+    }
+
+    public String getTelNum2() {
+        return telNum2;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+
+
+
+
+}
